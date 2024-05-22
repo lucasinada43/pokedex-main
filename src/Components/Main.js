de529d443a1245c0e98cba4729f61c472037762a
Lucas Gabriel da Cruz Inada <lucasinada43@gmail.com>
2024-05-22 15:41:07 -0300

    adicionando input

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,23 +3,25 @@ import { useEffect, useState } from "react";
 import { Div } from "./style";
 export function Main(){
     const [api, setApi] = useState({})
+    const [input, setInput] = useState({})
     useEffect(()=> {
         axios.get(`https://pokeapi.co/api/v2/pokemon/`).then(
         (response ) => {setApi(response.data.results)})
     },[])
     return(
-        <>
+                
+        <Div>
+          <input onInput={(parametro) => {setInput(parametro.target.value)}} type="text" />
+          {console.log(input)}
           {Object.keys(api).map(pokemon => {
             return (
-              <Div>
-              <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + (parseInt(pokemon) + 1) + ".png"}/> 
-                <div>{api[pokemon].name}
-                  
+              <>
+                <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + (parseInt(pokemon) + 1) + ".png"}/> 
+                <div>{api[pokemon].name} 
                 </div>
-              </Div>
+              </>
             )
-          })}
-          
-        </>
+          })}   
+        </Div>
     )
 }
--- src/Components/style.js
+++ src/Components/style.js
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 export const Div = styled.div`
-    display: flex;
-    align-items: center;
-    margin-left: 150px;
+    margin: auto;
+    margin-top: 150px; 
+    background-color: #b5b3b3;
+    width: 300px;
+    height: 400px;
+    overflow: auto;
+    border-radius: 15px;
+    padding: 20px;
+    padding-top: 40px;
+
 `
